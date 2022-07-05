00dd1736612555a7e9a63867ae4a07355d40b6d1
Edem <ziddah.edem@gmail.com>
2022-07-05 15:49:39 +0000

    updated

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -5,24 +5,20 @@ type Store = {
   authUser: IUser | null;
   uploadingImage: boolean;
   pageLoading: boolean;
-  openModal: boolean;
   setAuthUser: (user: IUser) => void;
   setUploadingImage: (isUploading: boolean) => void;
   setPageLoading: (isLoading: boolean) => void;
-  setOpenModal: (isOpen: boolean) => void;
 };
 
 const useStore = create<Store>((set) => ({
   authUser: null,
   uploadingImage: false,
   pageLoading: false,
-  openModal: false,
   setAuthUser: (user) => set((state) => ({ ...state, authUser: user })),
   setUploadingImage: (isUploading) =>
     set((state) => ({ ...state, uploadingImage: isUploading })),
   setPageLoading: (isLoading) =>
     set((state) => ({ ...state, pageLoading: isLoading })),
-  setOpenModal: (isOpen) => set((state) => ({ ...state, openModal: isOpen })),
 }));
 
 export default useStore;
